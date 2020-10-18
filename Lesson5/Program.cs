60025e9b58039e81892ae32d2e1906a50387ef60
Vadim <vadym@vadym.uk>
2020-10-18 16:05:46 +0100

    add lesson5

---
+++ Lesson5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace checkNumberOrText
+{
+    class Program
+    {
+        static int checkIfNumber (string str)
+        //Проверка введено число или текст. 
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsDigit(str[i]) == false)
+                {
+                    //введен текст
+                    return 0;
+                }
+            }
+                 //введено число
+                return 1;
+               }
+
+        static void Main(string[] args)
+        {
+                    Console.WriteLine("Введите число");
+                    string number = Console.ReadLine();
+           
+            int flag=  checkIfNumber (number);
+
+
+            if (flag == 1) Console.WriteLine($"Number= {number}");
+                    else
+        
+                Console.WriteLine($"Вы ввели текст");
+        }
+
+
+    }
+}
+
+
