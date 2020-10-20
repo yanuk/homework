4aa45a4d6e4cd72a5f8be8568e678175ea701818
Vadim <vadym@vadym.uk>
2020-10-20 21:16:59 +0100

    add essential 1

---
+++ essential1/Address.cs
@@ -0,0 +1,17 @@
+﻿namespace essential1
+{
+
+    using System;
+    using System.ComponentModel;
+    using System.Dynamic;
+
+    public class Address
+    {
+        public string Index { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
+        public string Apartment { get; set; }
+    }
+}
---
+++ essential1/Address1.cs
@@ -0,0 +1,58 @@
+﻿namespace essential1
+{
+    using System;
+    using System.ComponentModel;
+    using System.Dynamic;
+    public class Address1
+    {
+        private string index = null, country = null, city = null, street = null, house = null, apartment = null;
+        public void SetIndex(string value)
+        {
+            index = value;
+        }
+        public void SetCountry(string value)
+        {
+            country = value;
+        }
+        public void SetCity(string value)
+        {
+            city = value;
+        }
+        public void SetStreet(string value)
+        {
+            street = value;
+        }
+        public void SetHouse(string value)
+        {
+            house = value;
+        }
+        public void SetApartment(string value)
+        {
+            apartment = value;
+        }
+        public string GetApartment()
+        {
+            return (apartment);
+        }
+        public string GetHouse()
+        {
+            return (house);
+        }
+        public string GetCity()
+        {
+            return (city);
+        }
+        public string GetIndex()
+        {
+            return (index);
+        }
+        public string GetStreet()
+        {
+            return (street);
+        }
+        public string GetCountry()
+        {
+            return (country);
+        }
+    }
+}
