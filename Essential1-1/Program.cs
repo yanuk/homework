903ad8a085d20b94bb8c6409733482a6191f64f6
Vadim <72168260+yanuk@users.noreply.github.com>
2020-10-21 12:10:45 +0100

    add essential1-1

---
+++ Essential1-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Essential1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Введите длинну");
+            double wide = Convert.ToDouble (Console.ReadLine());
+            Console.WriteLine("Введите ширину");
+            double high = Convert.ToDouble(Console.ReadLine());
+                    
+            Rectangle inst = new Rectangle (wide, high);
+            double perimetr = inst.Perimetr;
+            double area = inst.Area;
+
+            Console.WriteLine($"area= {area} perimetr = { perimetr}");
+        }
+    }
+}
---
+++ Essential1-1/Rectangle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Essential1_1
+{
+    public class Rectangle
+    {
+        private double side1, side2;
+
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+
+        public double PerimeterCalculator()
+        {
+            return (2 * (this.side1 + this.side2));
+        }
+        public double AreaCalculator()
+        {
+            return (this.side1 * this.side2);
+        }
+
+        public double Perimetr
+        {
+            get
+            {
+                return (this.PerimeterCalculator());
+            }
+        }
+        public double Area
+        {
+            get
+            {
+                return (this.AreaCalculator());
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
